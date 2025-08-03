e8773d0ca589ad7cd62b184007053f9dc30e3d08
Kevin Erazo Romero <120631866+Kevinero25@users.noreply.github.com>
2025-08-03 15:48:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-import "bootstrap";
-import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let Pronombre= ["el", "la", "mi", "tu"]
+  let Adjetivo = ["genial", "rápido", "loco", "brillante"]
+  let Sustantivo =["perro", "sol", "código", "nube"]
+  let Dominio = [".es",".com"]
+
+
+
+  let boton = document.querySelector(".generar")
+  let contenedor = document.getElementById("container_header")
+
+  boton.onclick = function(){
+    contenedor.innerHTML="";
+
+    for(let i = 0;i < 10; i++){
+
+      //Random
+  let randomPronombre = Pronombre [Math.floor(Math.random()*Pronombre.length)]
+  let randomAdjetivo = Adjetivo [Math.floor(Math.random()*Adjetivo.length)]
+  let randomSustantivo = Sustantivo [Math.floor(Math.random()*Sustantivo.length)]
+  let randomDominio = Dominio [Math.floor(Math.random()*Dominio.length)]
+
+
+  let dominio = randomPronombre + randomAdjetivo + randomSustantivo + randomDominio;
+
+  let parrafo = document.createElement("p");
+  parrafo.textContent = dominio;
+  contenedor.appendChild(parrafo);
+  
+    }
+  }
 };
